167fdf5bd4604c5250a1404db6dd0bc14dd1fb65
RM-RITIK <mehtaritik11@gmail.com>
2022-01-02 01:38:14 +0530

    Final changes in Project List

--- src/components/project-list.js
+++ src/components/project-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ProjectDataService from "../services/project.service"
 import { CodeIcon } from "@heroicons/react/solid";
+import techStackImage from "../images/techStack.png"
 
 export default class ProjectList extends Component {
     constructor(props) {
@@ -45,32 +46,27 @@ export default class ProjectList extends Component {
                         My Projects
                     </h1>
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                        
+                        I have worked on various projects in my career so far in domains ranging from Web Development to Deep Learning
                     </p>
                     </div>
-                    <div className="flex flex-wrap -m-4">
+                    <div className="p-4 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 select-none">
                     {projects && projects.map((project, index) => (
-                        <a
-                        href={project.link}
-                        key={project.image}
-                        className="sm:w-1/2 w-100 p-4">
-                        <div className="flex relative">
-                            <img
-                            alt="gallery"
-                            className="absolute inset-0 w-full h-full object-cover object-center"
-                            src={project.imageUrl}
-                            />
-                            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                            <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                                {project.subtitle}
-                            </h2>
-                            <h1 className="title-font text-lg font-medium text-white mb-3">
-                                {project.title}
-                            </h1>
-                            <p className="leading-relaxed">{project.description}</p>
+                        <div class="w-full cursor-pointer rounded-md shadow-md shadow-gray-200 text-white hover:shadow-blue-400/80 hover:shadow-2xl hover:bg-gray-50 hover:text-black">
+                        <img class="object-cover w-full h-64" src={project.imageUrl} />
+                        <div class="p-4">
+                          <a href={project.link}><span class="text-blue-600 font-normal text-base">Link</span></a>
+                          <p class="font-normal text-base">{project.startTime} - {project.endTime}</p>
+                          <p class="font-semibold text-xl py-2">{project.title}</p>
+                          <p className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">{project.subtitle}</p>
+                          <p class="font-light text-gray-600 text-justify line-clamp-3">{project.description}</p>
+                          <div class="flex flex-wrap mt-10 space-x-4 align-bottom">
+                            <img class="w-10 h-10 rounded-full" src={techStackImage} />
+                            <div class="flex flex-col space-y-0">
+                              <p class="font-light text-sm">{project.techStack}</p>
                             </div>
+                          </div>
                         </div>
-                        </a>
+                      </div>
                     ))}
                     </div>
                 </div>
