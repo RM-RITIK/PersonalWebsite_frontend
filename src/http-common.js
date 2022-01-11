13888aec5583f76bf527f8fce656ecd359818010
RM-RITIK <mehtaritik11@gmail.com>
2022-01-11 10:20:48 +0530

    Some minor changes in the Website

--- src/http-common.js
+++ src/http-common.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "http://localhost:1337/api",
+  //baseURL: "https://stark-lowlands-42577.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
